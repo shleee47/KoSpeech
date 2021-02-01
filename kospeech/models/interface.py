f14561d10bdf81298d8b8f4e559330cf3b06ef8b
sanghoon <57610448+shleee47@users.noreply.github.com>
2021-02-01 14:37:38 +0900

    Update interface.py

--- kospeech/models/interface.py
+++ kospeech/models/interface.py
@@ -17,11 +17,20 @@ import torch.nn as nn
 from torch import Tensor
 from typing import Tuple
 
+from kospeech.models.modules import Linear
+from kospeech.models.encoder import (
+    BaseEncoder,
+    TransducerEncoder,
+)
+from kospeech.models.decoder import (
+    BaseDecoder,
+    TransducerDecoder,
+)
 
-class EncoderInterface(nn.Module):
-    """ ASR Encoder Interface for KoSpeech model implementation """
+
+class BaseModel(nn.Module):
     def __init__(self):
-        super(EncoderInterface, self).__init__()
+        super(BaseModel, self).__init__()
 
     def count_parameters(self) -> int:
         """ Count parameters of encoder """
@@ -33,67 +42,68 @@ class EncoderInterface(nn.Module):
             if isinstance(child, nn.Dropout):
                 child.p = dropout_p
 
-    def forward(self, inputs: Tensor, input_lengths: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
+    @torch.no_grad()
+    def recognize(self, inputs: Tensor, input_lengths: Tensor):
+        raise NotImplementedError
+
+
+class EncoderModel(BaseModel):
+    """ Super class of KoSpeech's Encoder only Models """
+    def __init__(self):
+        super(EncoderModel, self).__init__()
+        self.decoder = None
+
+    def set_decoder(self, decoder):
+        self.decoder = decoder
+
+    def forward(self, inputs: Tensor, input_lengths: Tensor) -> Tuple[Tensor, Tensor]:
         """
-        Forward propagate a `inputs` for  encoder training.
+        Forward propagate a `inputs` for  ctc training.
         Args:
             inputs (torch.FloatTensor): A input sequence passed to encoder. Typically for inputs this will be a padded
                 `FloatTensor` of size ``(batch, seq_length, dimension)``.
             input_lengths (torch.LongTensor): The length of input tensor. ``(batch)``
         Returns:
-            (Tensor, Tensor, Tensor):
-            * encoder_outputs: A output sequence of encoder. `FloatTensor` of size ``(batch, seq_length, dimension)``
-            * encoder_output_lengths: The length of encoder outputs. ``(batch)``
-            * encoder_log_probs: Log probability of encoder outputs will be passed to CTC Loss.
-                If joint_ctc_attention is False, return None.
+            (Tensor, Tensor):
+            * predicted_log_prob (torch.FloatTensor)s: Log probability of model predictions.
+            * output_lengths (torch.LongTensor): The length of output tensor ``(batch)``
         """
         raise NotImplementedError
 
-
-class DecoderInterface(nn.Module):
-    """ ASR Decoder Interface for KoSpeech model implementation """
-    def __init__(self):
-        super(DecoderInterface, self).__init__()
-
-    def count_parameters(self) -> int:
-        """ Count parameters of encoder """
-        return sum([p.numel for p in self.parameters()])
-
-    def update_dropout(self, dropout_p) -> None:
-        """ Update dropout probability of decoder """
-        for name, child in self.named_children():
-            if isinstance(child, nn.Dropout):
-                child.p = dropout_p
-
-    def forward(self, targets: Tensor, encoder_outputs: Tensor, **kwargs) -> Tensor:
-        """
-        Forward propagate a `encoder_outputs` for training.
-        Args:
-            targets (torch.LongTensr): A target sequence passed to decoder. `IntTensor` of size ``(batch, seq_length)``
-            encoder_outputs (torch.FloatTensor): A output sequence of encoder. `FloatTensor` of size
-                ``(batch, seq_length, dimension)``
-        Returns:
-            * predicted_log_probs (torch.FloatTensor): Log probability of model predictions.
-        """
-        raise NotImplementedError
 
     @torch.no_grad()
-    def decode(self, encoder_outputs: Tensor, *args) -> Tensor:
+    def decode(self, predicted_log_probs: Tensor) -> Tensor:
         """
         Decode encoder_outputs.
         Args:
-            encoder_outputs (torch.FloatTensor): A output sequence of encoder. `FloatTensor` of size
+            predicted_log_probs (torch.FloatTensor):Log probability of model predictions. `FloatTensor` of size
                 ``(batch, seq_length, dimension)``
         Returns:
             * predicted_log_probs (torch.FloatTensor): Log probability of model predictions.
         """
-        raise NotImplementedError
+        return predicted_log_probs.max(-1)[1]
+
+    @torch.no_grad()
+    def recognize(self, inputs: Tensor, input_lengths: Tensor) -> Tensor:
+        """
+        Recognize input speech.
+        Args:
+            inputs (torch.FloatTensor): A input sequence passed to encoder. Typically for inputs this will be a padded
+                `FloatTensor` of size ``(batch, seq_length, dimension)``.
+            input_lengths (torch.LongTensor): The length of input tensor. ``(batch)``
+        Returns:
+            * predictions (torch.FloatTensor): Result of model predictions.
+        """
+        predicted_log_probs, _ = self.forward(inputs, input_lengths)
+        if self.decoder is not None:
+            return self.decoder.decode(predicted_log_probs)
+        return self.decode(predicted_log_probs)
 
 
-class EncoderDecoderModelInterface(nn.Module):
-    """ Interface of KoSpeech's Encoder-Decoder Models """
-    def __init__(self, encoder: EncoderInterface, decoder: DecoderInterface) -> None:
-        super(EncoderDecoderModelInterface, self).__init__()
+class EncoderDecoderModel(BaseModel):
+    """ Super class of KoSpeech's Encoder-Decoder Models """
+    def __init__(self, encoder: BaseEncoder, decoder: BaseDecoder) -> None:
+        super(EncoderDecoderModel, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
         
@@ -150,80 +160,89 @@ class EncoderDecoderModelInterface(nn.Module):
         return self.decoder.decode(encoder_outputs, encoder_output_lengths)
 
 
-class CTCModelInterface(nn.Module):
-    """
-    Interface of KoSpeech's Speech-To-Text Models.
-    for a simple, generic encoder / decoder or encoder only model.
-    """
-    def __init__(self):
-        super(CTCModelInterface, self).__init__()
-        self.decoder = None
+class TransducerModel(BaseModel):
+    """ Super class of KoSpeech's Transducer Models """
+    def __init__(
+            self,
+            encoder: TransducerEncoder,
+            decoder: TransducerDecoder,
+            d_model: int,
+            num_classes: int,
+    ) -> None:
+        super(TransducerModel, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.fc = nn.Sequential(
+            Linear(d_model, d_model),
+            nn.Tanh(),
+            Linear(d_model, num_classes, bias=False),
+        )
+
+    def set_encoder(self, encoder):
+        """ Setter for encoder """
+        self.encoder = encoder
         
     def set_decoder(self, decoder):
+        """ Setter for decoder """
         self.decoder = decoder
 
     def count_parameters(self) -> int:
         """ Count parameters of encoder """
-        return sum([p.numel for p in self.parameters()])
+        num_encoder_parameters = self.encoder.count_parameters()
+        num_decoder_parameters = self.decoder.count_parameters()
+        return num_encoder_parameters + num_decoder_parameters
 
-    def update_dropout(self, dropout_p: float) -> None:
+    def update_dropout(self, dropout_p) -> None:
         """ Update dropout probability of model """
-        for name, child in self.named_children():
-            if isinstance(child, nn.Dropout):
-                child.p = dropout_p
-
-    def forward(self, inputs: Tensor, input_lengths: Tensor) -> Tuple[Tensor, Tensor]:
-        """
-        Forward propagate a `inputs` for  ctc training.
+        self.encoder.update_dropout(dropout_p)
+        self.decoder.update_dropout(dropout_p)
         Args:
-            inputs (torch.FloatTensor): A input sequence passed to encoder. Typically for inputs this will be a padded
-                `FloatTensor` of size ``(batch, seq_length, dimension)``.
-            input_lengths (torch.LongTensor): The length of input tensor. ``(batch)``
-        Returns:
-            (Tensor, Tensor):
-            * predicted_log_prob (torch.FloatTensor)s: Log probability of model predictions.
-            * output_lengths (torch.LongTensor): The length of output tensor ``(batch)``
-        """
-        raise NotImplementedError
+    def joint(self, encoder_outputs: Tensor, decoder_outputs: Tensor) -> Tensor:
+        input_length = encoder_outputs.size(1)
+        target_length = decoder_outputs.size(1)
+        encoder_outputs = encoder_outputs.unsqueeze(2)
+        decoder_outputs = decoder_outputs.unsqueeze(1)
+        encoder_outputs = encoder_outputs.repeat([1, 1, target_length, 1])
+        decoder_outputs = decoder_outputs.repeat([1, input_length, 1, 1])
+        
+        outputs = torch.cat((encoder_outputs, decoder_outputs), dim=-1)
+        outputs = self.fc(outputs)
+        
+        return outputs
+    
+    def forward(
+            self,
+            inputs: Tensor,
+            input_lengths: Tensor,
+            targets: Tensor,
+            target_lengths: Tensor
+    ) -> Tensor:
+        encoder_outputs, _ = self.encoder(inputs, input_lengths)
+        decoder_outputs, _ = self.decoder(targets, target_lengths)
+        outputs = self.joint(encoder_outputs, decoder_outputs)
+        return outputs
 
     @torch.no_grad()
-    def decode(self, predicted_log_probs: Tensor) -> Tensor:
-        """
-        Decode encoder_outputs.
-        Args:
-            predicted_log_probs (torch.FloatTensor):Log probability of model predictions. `FloatTensor` of size
-                ``(batch, seq_length, dimension)``
-        Returns:
-            * predictions (torch.FloatTensor): Result of model predictions.
-        """
-        return predicted_log_probs.max(-1)[1]
+    def decode(self, encoder_outputs: Tensor) -> Tensor:
+        predicted_log_probs, hidden_states = list(), None
+        
+        batch_size = encoder_outputs.size(0)
+        max_length = encoder_outputs.size(1)
+
+        decoder_inputs = encoder_outputs.new_tensor([self.decoder.sos_id] * batch_size, dtype=torch.long)
+
+        for t in range(max_length):
+            decoder_outputs, hidden_states = self.decoder(decoder_inputs, hidden_states)
+            step_outputs = self.joint(encoder_outputs, decoder_outputs)
+            step_outputs = step_outputs.log_softmax(dim=0)
+            predicted_log_probs.append(step_outputs)
+            decoder_inputs = predicted_log_probs[-1].topk(1)[1]
+
+        outputs = torch.stack(predicted_log_probs, dim=1)
+
+        return outputs
 
     @torch.no_grad()
-    def recognize(self, inputs: Tensor, input_lengths: Tensor) -> Tensor:
-        """
-        Recognize input speech.
-        Args:
-            inputs (torch.FloatTensor): A input sequence passed to encoder. Typically for inputs this will be a padded
-                `FloatTensor` of size ``(batch, seq_length, dimension)``.
-            input_lengths (torch.LongTensor): The length of input tensor. ``(batch)``
-        Returns:
-            * predictions (torch.FloatTensor): Result of model predictions.
-        """
-        predicted_log_probs, _ = self.forward(inputs, input_lengths)
-        if self.decoder is not None:
-            return self.decoder.decode(predicted_log_probs)
-        return self.decode(predicted_log_probs)
-
-class TransducerEncoderInterface(nn.Module):
-    def __init__(self):
-        super(TransducerEncoderInterface, self).__init__()
-
-
-class TransducerDecoderInterface(nn.Module):
-    def __init__(self):
-        super(TransducerDecoderInterface, self).__init__()
-
-
-class TransducerInterface(nn.Module):
-    def __init__(self):
-        super(TransducerInterface, self).__init__() 
+    def recognize(self, inputs: Tensor, input_lengths: Tensor):
+        encoder_outputs, _ = self.encoder(inputs, input_lengths)
+        return self.decode(encoder_outputs)
